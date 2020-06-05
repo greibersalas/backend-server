3db108bd4424d0da26e54278e609cf6060b178ee
Greiber Salas <greibersalas@gmsil.com>
2020-06-04 19:38:47 -0500

    Usuario y token

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
@@ -8,8 +9,18 @@ var mongoose = require('mongoose');
 var app = express();
 
 
+//Body Parser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
 //Conexión a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err,res)=>{
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalBD', (err,res)=>{
     if(err) throw err;
 
     console.log('Base de datos: \x1b[32m%s\x1b[0m',' online');
@@ -17,12 +28,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err,res)=>{
 
 
 //Rutas
-app.get('/',(req,res,next) =>{
-    res.status(200).json({
-        ok: true,
-        mensaje:'Petición realizada'
-    });
-});
+app.use('/usuario',usuarioRoutes);
+app.use('/login',loginRoutes);
+app.use('/',appRoutes);
+
 
 
 //Escuchar peticiones
